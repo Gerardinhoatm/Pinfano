0ab3c2b2af995dcbb1d617683ce2074e67fa8175
gerardinhoatm <gerardrojiblanco@gmail.com>
2026-02-03 13:18:40 +0100

    create class CreateGameHandler and template function CreateGameFunction

--- src/main/java/com/example/Pinfano/handlers/CreateGameHandler.java
+++ src/main/java/com/example/Pinfano/handlers/CreateGameHandler.java
@@ -23,7 +23,6 @@ public class CreateGameHandler implements RequestHandler<APIGatewayProxyRequestE
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
-
         if ("OPTIONS".equalsIgnoreCase(request.getHttpMethod())) {
             return new APIGatewayProxyResponseEvent()
                     .withStatusCode(200)
