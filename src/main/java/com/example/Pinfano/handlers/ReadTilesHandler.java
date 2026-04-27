a1849f440867f72e476f730771ff33b2b9f77305
gerardinhoatm <gerardrojiblanco@gmail.com>
2026-04-27 19:38:35 +0200

    changes

--- src/main/java/com/example/Pinfano/handlers/ReadTilesHandler.java
+++ src/main/java/com/example/Pinfano/handlers/ReadTilesHandler.java
@@ -12,36 +12,25 @@ import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 public class ReadTilesHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
     private static final String BUCKET = "pinfano-data";
-    private static final List<String> KEYS = List.of(
-            "fichas.json",
-            "fichasuno.json"
-    );
+    private static final String KEY = "fichasuno.json";
 
     private final S3Client s3 = S3Client.builder()
             .region(Region.EU_CENTRAL_1)
             .build();
 
-    private final Random random = new Random();
-
     @Override
     public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
 
         try {
-            // 🔀 Elige una key aleatoria
-            String chosenKey = KEYS.get(random.nextInt(KEYS.size()));
-            context.getLogger().log(">>> [ReadTilesHandler] KEY seleccionada: " + chosenKey);
-
             ResponseBytes<GetObjectResponse> fileBytes = s3.getObjectAsBytes(
                     GetObjectRequest.builder()
                             .bucket(BUCKET)
-                            .key(chosenKey)
+                            .key(KEY)
                             .build()
             );
 
@@ -54,7 +43,7 @@ public class ReadTilesHandler implements RequestHandler<APIGatewayV2HTTPEvent, A
                     .build();
 
         } catch (Exception e) {
-            context.getLogger().log(">>> [ReadTilesHandler] ERROR: " + e.getMessage() + "\n");
+            context.getLogger().log(">>> [ReadTilesHandler] ERROR leyendo JSON: " + e.getMessage() + "\n");
             return APIGatewayV2HTTPResponse.builder()
                     .withStatusCode(500)
                     .withBody("Error leyendo JSON: " + e.getMessage())
